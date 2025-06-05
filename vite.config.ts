3ad2df229f72912db34cc181fd9e24973bc54cca
leesuyoen <dudgk0216@naver.com>
2025-06-05 19:40:03 +0900

    fix :vercel오류 수정중

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-
-
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
+  resolve: {
+    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json"],
+  },
 });
